435fa98f7478ca7639745226b37f222418cffc55
Matthew Lam <Matthew@calvisitor-10-105-184-77.calvisitor.1918.berkeley.edu>
2018-11-03 02:55:41 -0700

    first draft :)

---
+++ lul.js
@@ -0,0 +1,9 @@
+function simpleClick() {
+	var popup = document.getElementById("simpleLibrary");
+    popup.classList.toggle("show");
+}
+
+function seeMore() {
+	var popup = document.getElementById("moreLibrary");
+	popup.classList.toggle("show");
+}
