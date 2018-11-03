a0e3d5e74ade4f9cb796fcb65708084aa853ee40
Matthew Lam <Matthew@calvisitor-10-105-184-77.calvisitor.1918.berkeley.edu>
2018-11-03 04:50:37 -0700

    finished animation

--- lul.js
+++ lul.js
@@ -1,9 +1,26 @@
 function simpleClick() {
 	var popup = document.getElementById("simpleLibrary");
-    popup.classList.toggle("show");
+    if (popup.style.opacity == 0) {
+    popup.classList.remove("fade");
+    popup.classList.add("show");
+    popup.style.opacity = 1;
+    } else {
+    popup.classList.remove("show");
+    popup.classList.add("fade");
+    popup.style.opacity = 0;
+    }
 }
 
+function myFunction() {
+    
+    
+}
 function seeMore() {
 	var popup = document.getElementById("moreLibrary");
-	popup.classList.toggle("show");
+	if (popup.visibility == "hidden") {
+		popup.classList.toggle("show");
+	} else {
+		popup.classList.toggle("fade");
+	}
+	
 }
