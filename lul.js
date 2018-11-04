e343a571d9c52e52ef9a5d69633d5b07446b156c
Matthew Lam <Matthew@airbears2-10-142-74-56.airbears2.1918.berkeley.edu>
2018-11-03 19:44:18 -0700

    finished animation

--- lul.js
+++ lul.js
@@ -1,5 +1,19 @@
-function simpleClick() {
-	var popup = document.getElementById("simpleLibrary");
+function simpleLibrary(library, image, hours, seats, capacity, day, sound, food) {
+    var popup = document.getElementById("myPopup");
+    var soundimage = '';
+    var foodimg = '';
+    if (sound) {
+    soundimage = 'https://i.imgur.com/d4fQUV5.png';
+    } else {
+    soundimage = 'https://i.imgur.com/QuEuwB4.png';
+    }
+    if (food) {
+    foodimg = 'https://i.imgur.com/J896HT9.png';
+    } else {
+    foodimg = 'https://i.imgur.com/eZpInTj.png';
+    }
+    var text = '<img src =' + image + ' width = "100%" height = "auto"><p style = "text-align: left; line-height: 5px;"><span style="font-size: 13pt; font-family: "book antiqua", palatino;"><strong>&nbsp;&nbsp;&nbsp;' + library + '&nbsp;</strong></span></p><p style = "text-align: left;line-height: 5px;"><span style="font-size: 10pt; font-family: "book antiqua", palatino;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src =' + soundimage + ' width = "7%" height = "auto">&nbsp;<img src = ' + foodimg + ' width = "7%" height = "auto">&nbsp;'+ day + '&nbsp;Hours: ' + hours + '</p><p style = "text-align: left;line-height: 10px;"><span style="font-family: "book antiqua", palatino;"><strong>&nbsp;&nbsp;&nbsp;Open Seats:&nbsp;' + seats + '&nbsp;</strong></span></p><p style = "text-align: left;line-height: 5px;"><span style="font-family: "book antiqua", palatino;"><strong>&nbsp;&nbsp;&nbsp;Capacity:&nbsp;' + capacity + '&nbsp;full</strong></span></p><p style = "text-align: right; line-height: 5px;><span style="font-size: 13pt; font-family: "book antiqua", palatino;"><strong> See More &nbsp;&nbsp;&nbsp;&nbsp; </p>';
+    popup.innerHTML = text;
     if (popup.style.opacity == 0) {
     popup.classList.remove("fade");
     popup.classList.add("show");
@@ -9,12 +23,7 @@ function simpleClick() {
     popup.classList.add("fade");
     popup.style.opacity = 0;
     }
-}
 
-function myFunction() {
-    
-    
-}
 function seeMore() {
 	var popup = document.getElementById("moreLibrary");
 	if (popup.visibility == "hidden") {
